cbbe23108347155ed8a120b4e5c4abd3fe2e749e
abdikarim yusuf <abdikarimyusuf6@gmail.com>
2025-06-25 22:41:06 +0100

    multi versioning

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ import unittest
 
 from app import say_hello
 
-class test_hello(unittest.Testcase):
+class test_hello(unittest.TestCase):
     def test_say_hello(self):
         self.asserEqual(say_hello("abdi"),"abdi")
 
